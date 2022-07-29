23deff8d9700312eb9dd60aebcee1f18e2c11b1b
Roman Benin <romaben@bk.ru>
2022-07-29 19:00:09 +0300

    Code improvement

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,23 +1,11 @@
 public class CreditPaymentService {
-    void calculate(int creditTerm, double creditSum) {
-        double payPerMonth, percent = 9.99;
-        int monthSum = 12;
-        System.out.println("Сумма кредита       |   " + (int)creditSum);
-        if (creditTerm < 2) {
-            System.out.println("Срок кредита        |   " + creditTerm + " год");
-        } else {
-            System.out.println("Срок кредита        |   " + creditTerm + " года");
-        }
+    double calculate(int creditTerm, int monthSum, double creditSum, double percent) {
         monthSum = monthSum * creditTerm;
-        creditSum = 1000000;
         double monthPercent = percent / 12 / 100;
         double monthPercentPlusOne = monthPercent + 1.0;
         double powedOnePlusPMP = Math.pow(monthPercentPlusOne, monthSum);
         double annuitantK = monthPercent * powedOnePlusPMP / (powedOnePlusPMP - 1);
-        payPerMonth = creditSum * annuitantK;
-
-        System.out.println("Ежемесячный платёж  |   " + (int)payPerMonth +
-                "\nПроцентная ставка   |   " + percent + "%");
-        System.out.println("________________________________");
+        double payPerMonth = creditSum * annuitantK;
+        return payPerMonth;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,22 @@ public class Main {
 
         CreditPaymentService service = new CreditPaymentService();
 
-        int creditTerm = 1;
-        double creditSum = 1000000;
-        service.calculate(creditTerm, creditSum);
-        creditTerm++;
-        service.calculate(creditTerm, creditSum);
-        creditTerm++;
-        service.calculate(creditTerm, creditSum);
+        int creditTerm = 1, monthSum = 12;
+        double creditSum = 1000000, percent = 9.99;
+        int payPM;
+
+        while (creditTerm < 4) {
+            System.out.println("Сумма кредита       |   " + (int)creditSum);
+            if (creditTerm < 2) {
+                System.out.println("Срок кредита        |   " + creditTerm + " год");
+            } else {
+                System.out.println("Срок кредита        |   " + creditTerm + " года");
+            }
+            payPM = (int) service.calculate(creditTerm, monthSum, creditSum, percent);
+            System.out.println("Ежемесячный платёж  |   " + payPM +
+                    "\nПроцентная ставка   |   " + percent + "%");
+            System.out.println("________________________________");
+            creditTerm++;
+        }
     }
 }
